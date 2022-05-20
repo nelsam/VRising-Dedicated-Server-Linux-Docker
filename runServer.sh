89f74e6d2c90b3fcad382166bb132096cc49f444
smb1982 <78674831+smb1982@users.noreply.github.com>
2022-05-20 20:19:27 +1000

    Update runServer.sh

--- runServer.sh
+++ runServer.sh
@@ -6,3 +6,5 @@ sudo docker run --name "VRisingUpdater" --rm -it -v $PWD/data:/data honestventur
 cp data/VRisingServer_Data/StreamingAssets/Settings/ServerHostSettings.json data/save-data/ServerHostSettings.json
 cp data/VRisingServer_Data/StreamingAssets/Settings/ServerGameSettings.json data/save-data/ServerGameSettings.json
 sudo docker run -it -d --name vRisingServer --rm -v $PWD/data:/data -p 27015:27015 -p 27016:27016 -p 27015:27015/udp -p 27016:27016/udp vrisingdedserver/wine64:v20052022 /bin/bash -c "cd /data; /usr/bin/xvfb-run wine VRisingServer.exe -persistentDataPath \".\save-data\" -log"
+rm ./runServer.sh
+mv ./runServer.tmp ./runServer.sh
