36cd74dce92cfcaa8fbd4fde800e00707aa7f7aa
Samuel Nelson <valczir.darkvein@gmail.com>
2022-05-23 11:33:29 -0500

    run-server.sh: copy config files back and forth
    This is needed because the devs have marked the server config file as
    part of the game data, so when steam detects a change to the config files,
    it will overwrite them.

--- run-server.sh
+++ run-server.sh
@@ -7,6 +7,27 @@ appID=1829350
 rconPort=${VRISING_SERVER_RCON_PORT:-27015}
 gamePort=${VRISING_SERVER_GAME_PORT:-27016}
 image=${VRISING_SERVER_IMAGE:-vrisingdedserver/wine64}
+cfgDir=${VRISING_SERVER_CONFIG_DIR:-data/save-data}
+
+## writeCfg writes a user's config to the game config directory. This has to be
+## done every time we validate files because the game will overwrite any
+## customizations that have been made.
+writeCfg() {
+    dataDir="data/VRisingServer_Data/StreamingAssets/Settings"
+    cfgFile="${1}"
+    if [ -e "${cfgDir}/${cfgFile}" ]
+    then
+        cp "${cfgDir}/${cfgFile}" "${dataDir}/${cfgFile}"
+    fi
+}
+
+## syncConfig syncs back a server's config to the user's config directory. Most
+## of the time, the file won't have changed; but it's better to be safe.
+syncCfg() {
+    dataDir="data/VRisingServer_Data/StreamingAssets/Settings"
+    cfgFile="${1}"
+    cp "${dataDir}/${cfgFile}" "${cfgDir}/${cfgFile}"
+}
 
 if [ ! -e data ]
 then
@@ -24,6 +45,9 @@ then
     fi
 fi
 
+hostCfg="ServerHostSettings.json"
+gameCfg="ServerGameSettings.json"
+
 docker run --interactive --tty --rm --user $(id -u):$(id -g) \
     --name "VRisingUpdater" \
     --volume $PWD/data:/data \
@@ -33,8 +57,8 @@ docker run --interactive --tty --rm --user $(id -u):$(id -g) \
     +app_update ${appID} validate \
     +quit
 
-cp data/VRisingServer_Data/StreamingAssets/Settings/ServerHostSettings.json data/save-data/ServerHostSettings.json
-cp data/VRisingServer_Data/StreamingAssets/Settings/ServerGameSettings.json data/save-data/ServerGameSettings.json
+writeCfg $hostCfg
+writeCfg $gameCfg
 
 docker run --interactive --tty --detach --rm --user $(id -u):$(id -g) \
     --name "VRisingServer" \
@@ -44,3 +68,6 @@ docker run --interactive --tty --detach --rm --user $(id -u):$(id -g) \
     --publish ${rconPort}:27015/udp \
     --publish ${gamePort}:27016/udp \
     ${image}:${version}
+
+syncCfg $hostCfg
+syncCfg $gameCfg
